bcf4739f92c2f504ddc56f23644d6cfdf95d5b26
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-13 19:36:23 +0100

    add class to component

--- src/js/views/FirstComponent.js
+++ src/js/views/FirstComponent.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class FirstComponent extends React.Component {
   render() {
-    return <h1>Welcome to this app's first component</h1>;
+    return <h1 className='firstComponent'>Welcome to this app's first component</h1>;
   }
 }
