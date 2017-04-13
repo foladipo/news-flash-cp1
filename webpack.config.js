8572b70dbeeb52c0c1d4530ea455eba389ad6623
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-13 12:01:35 +0100

    convert scss to css and save result in a separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,41 @@
-module.exports = {
+/* We will use ExtractTextPlugin to our transpiled 
+styling rules into a separate file. */
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractCssToFile = new ExtractTextPlugin('styles/style.css');
+
+var webpackConfig = {
   entry: './src/js/main.js',
   output: {
-    path: __dirname + '/dist/scripts/',
-    filename: 'bundle.js'
+    path: __dirname + '/dist/',
+    filename: 'scripts/bundle.js',
+    publicPath: 'dist/'
   },
   module: {
     loaders: [
       {
+        // ES6 to ES5 conversion using Babel.
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        // Convert .scss files to .css.
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: extractCssToFile.extract({
+          use: ['css-loader', 'sass-loader'],
+
+          // In case we can't extract our CSS into a separate file, we'll 
+          // just load it load it inline in our JS files, as is default.
+          fallback: 'style-loader'
+        })
       }
     ]
-  }
-}
+  },
+  plugins: [
+    extractCssToFile
+  ]
+};
+
+module.exports = webpackConfig;
