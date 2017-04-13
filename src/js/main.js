a98c74d6ab959b7ab8c7d52042cc96e9bc1ba931
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-13 12:06:04 +0100

    import a custom stylesheet

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FirstComponent from './views/FirstComponent';
 
+// Custom styles.
+import SiteWideStyles from '../css/siteWide.scss';
+
 ReactDOM.render(<FirstComponent/>, document.getElementById('target'));
