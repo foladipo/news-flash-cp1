443b78ba6ba7edc05e1782865ad2ba57b5669429
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-14 15:57:18 +0100

    delete component created for project setup purposes

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FirstComponent from './views/FirstComponent';
 
 // Custom styles.
 import SiteWideStyles from '../css/siteWide.scss';
-
-ReactDOM.render(<FirstComponent />, document.getElementById('target'));
--- src/js/views/FirstComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class FirstComponent extends React.Component {
-  render() {
-    return <h1 className='firstComponent'>Welcome to this app's first component</h1>;
-  }
-}
