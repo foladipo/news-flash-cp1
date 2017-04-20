7f026f2ba8c1f9767d76d5932bd240a0a21472c7
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-20 09:04:18 +0100

    expose firebase-initializing method to the DOM window

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@ import ReactDOM from 'react-dom';
 
 // Custom styles.
 import SiteWideStyles from '../css/siteWide.scss';
+
+// Add custom scripts that aren't relevant to React or the Flux pattern.
+import FirebaseSetup from './other/FirebaseSetup';
+const initFirebase = FirebaseSetup.initFirebase;
+// Expose this method as a global method.
+window.initFirebase = initFirebase;
