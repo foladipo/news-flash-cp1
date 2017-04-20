b0c53402851fbe90af313190ff2deb9d07d3a6d7
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-20 17:10:46 +0100

    fix bug regarding the template rendered for this route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
     pageTitle: 'Dashboard | NewsFlash',
     pageMessage: 'Your News Headlines'
   };
-  res.render('index', placeholderValues);
+  res.render('dashboard', placeholderValues);
 });
 
 module.exports = router;
