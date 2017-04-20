29910290a731f56c2e93807da8954f4df9baaf5b
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-20 18:35:45 +0100

    import functions for signing in or out

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,9 @@ import SiteWideStyles from '../css/siteWide.scss';
 
 // Add custom scripts that aren't relevant to React or the Flux pattern.
 import FirebaseSetup from './other/FirebaseSetup';
+import * as homePage from './other/homePage';
+import * as utilityFunctions from './other/utilityFunctions';
+
 const initFirebase = FirebaseSetup.initFirebase;
 // Expose this method as a global method.
 window.initFirebase = initFirebase;
