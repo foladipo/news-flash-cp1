97bcc963a830d04fa449bb6608c1364d51bea84a
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-21 07:57:34 +0100

    return key due to a) a bug in using dotenv to import it and b) it is not a security risk

--- src/js/other/FirebaseSetup.js
+++ src/js/other/FirebaseSetup.js
@@ -1,12 +1,10 @@
 // Assumes the Firebase client and its desired features have been loaded.
 // Only the Firebase features needed at the moment are included.
 const firebase = require('firebase');
-const dotenv = require('dotenv');
-dotenv.config();
 
 exports.initFirebase = () => {
   const config = {
-      apiKey: process.env.FIREBASE_API_KEY,
+      apiKey: 'AIzaSyCYUbZwe5kniitccbY06AWMMMu3oKEAOMw',
       authDomain: "news-flash-cp1.firebaseapp.com",
       databaseURL: "https://news-flash-cp1.firebaseio.com",
       projectId: "news-flash-cp1"
