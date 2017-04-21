d15200c5e0766a9300ba70a87c7cc54494757512
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-21 18:45:45 +0100

    wrap the DashboardAppContainer component and display it

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DashboardAppContainer from './views/DashboardAppContainer';
 
 // Custom styles.
 import SiteWideStyles from '../css/siteWide.scss';
@@ -12,3 +13,19 @@ import * as utilityFunctions from './other/utilityFunctions';
 const initFirebase = FirebaseSetup.initFirebase;
 // Expose this method as a global method.
 window.initFirebase = initFirebase;
+
+class WrappedApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="col-lg-2 col-md-1 col-sm-0"></div>
+        <DashboardAppContainer />
+        <div className="col-lg-2 col-md-1 col-sm-0"></div>
+      </div>
+    );
+  }
+}
+
+if (window.location.pathname === '/dashboard') {
+  ReactDOM.render(<WrappedApp />, document.getElementById('wrapper'));
+}
