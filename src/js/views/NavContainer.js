ec710525c19cd930cf4b8c6e20b719c5476129c6
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-21 12:30:05 +0100

    write code to satisfy the specs for the NavContainer component

--- src/js/views/NavContainer.js
+++ src/js/views/NavContainer.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 export default class NavContainer extends React.Component {
-    render() {
-        return <h1>Empty</h1>;
-    }
+  render() {
+    return (
+      <div id='nav-container' className='row'>
+        <h1>This is the nav container.</h1>
+      </div>
+    );
+  }
 }
