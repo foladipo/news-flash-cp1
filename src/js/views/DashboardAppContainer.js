a20d5909d5d6f3c7c4a016af949e34540dd3a0e9
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-21 13:40:43 +0100

    write code to satisfy the specs for the DashboardAppContainer component

--- src/js/views/DashboardAppContainer.js
+++ src/js/views/DashboardAppContainer.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import ArticlesContainer from './ArticlesContainer';
+import FooterContainer from './FooterContainer';
+import NavContainer from './NavContainer';
 
 export default class DashboardAppContainer extends React.Component {
-    render() {
-        return <h1>Empty</h1>;
-    }
+  render() {
+    return (
+      <div id='app-container' className='col-lg-8 col-md-10 col-sm-12'>
+        <NavContainer />
+        <ArticlesContainer />
+        <FooterContainer />
+      </div>
+    );
+  }
 }
