c908f87a8e9dfea94cba54a9251f92f1c03853ba
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-21 19:00:20 +0100

    remove redundant code and improve code style

--- src/js/other/FirebaseSetup.js
+++ src/js/other/FirebaseSetup.js
@@ -3,20 +3,11 @@
 const firebase = require('firebase');
 
 exports.initFirebase = () => {
-  console.log('called');
-  // const config = {
-  //   apiKey: "AIzaSyDTLsprvv9GcYCmMHygGph-fXsA1BvEwF4",
-  //   authDomain: "document-tracker-cohort-20.firebaseapp.com",
-  //   databaseURL: "https://document-tracker-cohort-20.firebaseio.com",
-  //   projectId: "document-tracker-cohort-20",
-  //   storageBucket: "document-tracker-cohort-20.appspot.com",
-  //   messagingSenderId: "409648985148"
-  // };
   const config = {
       apiKey: 'AIzaSyCYUbZwe5kniitccbY06AWMMMu3oKEAOMw',
-      authDomain: "news-flash-cp1.firebaseapp.com",
-      databaseURL: "https://news-flash-cp1.firebaseio.com",
-      projectId: "news-flash-cp1"
+      authDomain: 'news-flash-cp1.firebaseapp.com',
+      databaseURL: 'https://news-flash-cp1.firebaseio.com',
+      projectId: 'news-flash-cp1'
   };
   firebase.initializeApp(config);
   return firebase;
