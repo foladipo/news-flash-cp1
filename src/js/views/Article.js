a48b35d15946cbc4b09aba54d3fc664c825b5d39
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-22 17:34:37 +0100

    implement the Article component to satisfy existing specs

--- src/js/views/Article.js
+++ src/js/views/Article.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 export default class Article extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <h1>Empty</h1>;
+    const { id, title, description, url, imageUrl, author, publishedAt } = this.props;
+    
+    return (
+      <div className='row'>
+        <div className='col-md-2'>
+          <a href={url} target='_blank'><img src={imageUrl}/></a>
+        </div>
+        <div className='col-md-10'>
+          <a href={url} target='_blank'><span>{description}</span></a>
+          <p>{description}</p>
+          <div>
+            <span>Published at: {publishedAt}</span>
+            <span>Written by: {author}</span>
+          </div>
+          <span className='btn btn-info'>{id}</span>
+        </div>
+      </div>
+    );
   }
 }
