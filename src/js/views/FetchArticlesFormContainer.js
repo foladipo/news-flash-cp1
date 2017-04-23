c194d2fb2daf9b7824f2c3b617993f3a567c71b1
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-23 22:58:28 +0100

    fire actions as this form is used to specify news source etc

--- src/js/views/FetchArticlesFormContainer.js
+++ src/js/views/FetchArticlesFormContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as Utilities from '../constants/Utilities';
+import * as ArticlesActions from '../actions/ArticlesActions';
 
 export default class FetchArticlesFormContainer extends React.Component {
   constructor(props) {
@@ -7,6 +8,22 @@ export default class FetchArticlesFormContainer extends React.Component {
     this.state = {
       newsSources: Utilities.getAllNewsSources()
     };
+  }
+
+  handleNewsSourceChange(event) {
+    const sourceId = event.target.value;
+    this.state.sourceId = sourceId;
+    ArticlesActions.newsSourceChange(sourceId);
+  }
+
+  handleSortChange(event) {
+    const sortBy = event.target.value;
+    this.state.sort = sortBy;
+    ArticlesActions.sortChange(sortBy);
+  }
+
+  handleFetchArticles(event) {
+    ArticlesActions.startFetchArticles(this.state.sourceId, this.state.sort);
   }
 
   render() {
@@ -17,15 +34,15 @@ export default class FetchArticlesFormContainer extends React.Component {
     return (
       <div id='fetch-articles-form-container' className='col-md-6'>
         <div id='choose-news-source-container' className='col-xs-4'>
-          <select id='choose-news-source' className='form-control'>
+          <select id='choose-news-source' className='form-control' onChange={this.handleNewsSourceChange.bind(this)}>
             {newsSourcesOptions}
           </select>
         </div>
         <div id='choose-sort-container' className='col-xs-4'>
-          <select id='choose-sort' className='form-control' disabled></select>
+          <select id='choose-sort' className='form-control' disabled onChange={this.handleSortChange.bind(this)}></select>
         </div>
         <div id='fetch-articles-btn-container' className='col-xs-4'>
-          <button id='fetch-articles-btn' className='btn btn-primary' disabled>Get News</button>
+          <button id='fetch-articles-btn' className='btn btn-primary' disabled onClick={this.handleFetchArticles.bind(this)}>Get News</button>
         </div>
       </div>
     );
