ed8e5204f73fe571493b099a8f5312139643b340
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-23 13:09:15 +0100

    add code to NavContainer to fulfill all specs

--- src/js/views/NavContainer.js
+++ src/js/views/NavContainer.js
@@ -4,7 +4,12 @@ export default class NavContainer extends React.Component {
   render() {
     return (
       <div id='nav-container' className='row'>
-        <h1>This is the nav container.</h1>
+        <div id='nav-controls' className='col-md-6'>
+
+        </div>
+        <div id='fetch-articles-form-container' className='col-md-6'>
+
+        </div>        
       </div>
     );
   }
