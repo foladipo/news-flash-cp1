aa846b09b7f1c2a768c3b3581d825ffd6e51e9fe
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-23 14:22:12 +0100

    create spec file for FetchArticlesFormContainer and add some tests

---
+++ src/js/views/FetchArticlesFormContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class FetchArticlesFormContainer extends React.Component {
+  render() {
+    return (
+      <h1>Fetch articles form</h1>
+    );
+  }
+}
