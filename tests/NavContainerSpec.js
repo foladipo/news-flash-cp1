fd5391f1b3f3b860bb2d45bf984dc4dd9ef35e17
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-23 12:56:45 +0100

    add new specs for NavContainer

--- tests/NavContainerSpec.js
+++ tests/NavContainerSpec.js
@@ -23,4 +23,20 @@ describe('NavContainer', () => {
   it('should have a class of row', () => {
     expect(wrapper).to.have.className('row');
   });
+
+  it('should have two direct children', () => {
+    expect(wrapper.children()).to.have.length(2);
+  });
+
+  it('should have two direct children with class col-md-6', () => {
+    expect(wrapper.children().every('.col-md-6')).to.equal(true);
+  });
+
+  it('should have a first child with an id of nav-controls', () => {
+    expect(wrapper.childAt(0)).to.have.id('nav-controls');
+  });
+
+  it('should have a second child with an id of fetch-articles-form-container', () => {
+    expect(wrapper.childAt(1)).to.have.id('fetch-articles-form-container');
+  });
 });
