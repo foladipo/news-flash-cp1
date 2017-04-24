22ecc682e7a03335cd9fc767a94f753e27aa8956
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-24 20:43:05 +0100

    add a spec file for the ArrivalMessage component

---
+++ src/js/views/ArrivalMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class ArrivalMessage extends React.Component {
+  render() {
+    return <h1>Empty</h1>;
+  }
+}
---
+++ tests/ArrivalMessageSpec.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { mount } from 'enzyme';
+import ArrivalMessage from '../src/js/views/ArrivalMessage';
+
+chai.use(chaiEnzyme());
+const expect = chai.expect;
+
+describe('ArrivalMessage', () => {
+  it('should be defined', () => {
+    expect(ArrivalMessage).to.not.equal(undefined);
+  });
+});
+
+const wrapper = mount(<ArrivalMessage />);
+const children = wrapper.children();
+describe('ArrivalMessage', () => {
+  it('should have a class of row when rendered', () => {
+    expect(wrapper).to.have.className('row');
+  });
+
+  it('should have exactly two (2) direct children', () => {
+    expect(children).to.have.length(2);
+  });
+
+  const firstChild = children.at(0);
+  it('should have a first child that is an h1', () => {
+    expect(firstChild.is('h1')).to.equal(true);
+  });
+
+  it('should have a first child that is a class of display-4', () => {
+    expect(firstChild.is('.display-4')).to.equal(true);
+  });
+
+  it('should have a first child that is a class of text-center', () => {
+    expect(firstChild.is('.text-center')).to.equal(true);
+  });
+
+  it('should have a first child that is a class of bg-success', () => {
+    expect(firstChild.is('.bg-success')).to.equal(true);
+  });
+
+  // For details of why I decided to use render(), see:
+  // http://airbnb.io/enzyme/docs/api/ReactWrapper/text.html#-text-string
+  it('should have a first child that has the text \'Welcome to your dashboard\'', () => {
+    expect(firstChild.render().text()).to.equal('Welcome to your dashboard');
+  });
+
+  const secondChild = children.at(1);
+  it('should have a second child that is a p element', () => {
+    expect(secondChild.is('p')).to.equal(true);
+  });
+
+  it('should have a second child that has a class of lead', () => {
+    expect(secondChild.is('.lead')).to.equal(true);
+  });
+
+  // For details of why I decided to use render(), see:
+  // http://airbnb.io/enzyme/docs/api/ReactWrapper/text.html#-text-string
+  const welcomeText = 'To read news articles from your desired source, please use ' +
+    'the form in the navigation bar.';
+  it(`should have a second child that has text of '${welcomeText}'`, () => {
+    expect(secondChild.render().text()).to.equal(welcomeText);
+  });
+});
