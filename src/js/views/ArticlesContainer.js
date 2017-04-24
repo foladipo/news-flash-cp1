b9bbd3db0c7c4880df1b8e213101932c536a21cf
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-24 08:31:05 +0100

    listen for updates in ArticlesStore and react as is appropriate

--- src/js/views/ArticlesContainer.js
+++ src/js/views/ArticlesContainer.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import Article from './Article';
+import ArticlesStore from '../stores/ArticlesStore';
+import * as ArticlesActions from '../actions/ArticlesActions';
 
 export default class ArticlesContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: ArticlesStore.articles
+    }
+  }
+
+  componentWillMount() {
+    ArticlesStore.on('loadArticles', () => {
+      this.setState({
+        articles: ArticlesStore.articles
+      });
+    })
+  }
+
+  fetchArticles() {
+    ArticlesActions.startFetchArticles('cnn', 'top');
+  }
+
   render() {
+    const Articles = this.state.articles.map(article => {
+      // Use the spread operator to extract each key in the article object.
+      return <Article key={article.id} {...article} />;
+    });
     return (
-      <div id="articles-container" className="row">
+      <div id='articles-container' className='row'>
         <h1>This is the articles container.</h1>
+        <button onClick={this.fetchArticles.bind(this)} id='foobar'>Fetch Articles</button>
+        <div>{Articles}</div>
       </div>
     );
   }
 }
+
+window.ArticleStore = ArticlesStore;
