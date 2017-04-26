86565c8532848e11bf7a8bc41cef8e80535ef368
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-26 18:07:17 +0100

    write code to meet the specs for AppStatusContainer

--- src/js/views/AppStatusContainer.js
+++ src/js/views/AppStatusContainer.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-export default class ArrivalMessage extends React.Component {
+export default class AppStatusContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      htmlClasses: ['row']
+    };
+  }
+
   render() {
-    return <h1>Empty</h1>;
+    const currentHtmlClasses = this.state.htmlClasses.join(' ');
+    return (
+      <div className={currentHtmlClasses}>
+        <img src='/img/welcome-img.png' className='img-responsive center-block'/>
+        <h1 className='display-4 text-center bg-light-blue'>Welcome to your dashboard</h1>
+        <p className='lead text-center'>To read news articles from your desired source, please use the form in the navigation bar.</p>
+      </div>
+    );
   }
 }
