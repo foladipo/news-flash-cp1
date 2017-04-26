ac98e357e7f315d6d0517d4e4695065a63ffc43e
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-26 20:35:08 +0100

    satisfy the specs for AppStatusContainer

--- src/js/views/AppStatusContainer.js
+++ src/js/views/AppStatusContainer.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default class AppStatusContainer extends React.Component {
   render() {
     return (
-      <h1>Empty</h1>
+      <div id='app-status-container' className='hidden'>
+        <img src='/img/loading-img.png' id='loading-articles' className='img-responsive center-block'/>
+        <img src='/img/error-loading-img.png' id='error-loading-articles' className='img-responsive center-block hidden'/>
+        <h3></h3>
+      </div>
     );
   }
 }
