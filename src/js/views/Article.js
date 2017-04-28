0fb94df7962b8272a7c8645f6d00421590bed337
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-28 19:44:35 +0100

    add a dropdown button for sharing to social networks

--- src/js/views/Article.js
+++ src/js/views/Article.js
@@ -7,6 +7,9 @@ export default class Article extends React.Component {
 
   render() {
     const { id, title, description, url, imageUrl, author, publishedAt } = this.props;
+    const facebookShareLink = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
+    const googleShareLink = `https://plus.google.com/share?url=${url}`;
+    const twitterShareLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(title)}&url=${url}&`;
     
     let publishDate = '';
     if (publishedAt) {
@@ -27,6 +30,18 @@ export default class Article extends React.Component {
           </div>
           <p className='article-description'>{description}</p>
           <div>
+            <div className='col-xs-12'>
+              <div className='btn-group pull-right'>
+                <button type='button' className='btn btn-success dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                  Share <span className='caret'></span>
+                </button>
+                <ul className='dropdown-menu'>
+                  <li><a href={facebookShareLink} target='_blank'>Facebook</a></li>
+                  <li><a href={googleShareLink} target='_blank'>Google+</a></li>
+                  <li><a href={twitterShareLink} target='_blank'>Twitter</a></li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
