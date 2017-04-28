593958d4ade588af9bae5faac19c293e34a3f71e
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-28 12:57:49 +0100

    style the nav bar better and move the sign out button into it

--- src/js/views/NavContainer.js
+++ src/js/views/NavContainer.js
@@ -5,8 +5,17 @@ export default class NavContainer extends React.Component {
   render() {
     return (
       <div id='nav-container' className='row'>
-        <div id='nav-controls' className='col-md-6'>
-
+        <div id='nav-controls' className='col-md-12'>
+          <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              <ul className="nav navbar-nav navbar-left"></ul>
+              <ul className="nav navbar-nav navbar-right">
+                <li>
+                  <button id="sign-out-btn" className="btn btn-info navbar-btn">Sign out</button>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </div>
         <FetchArticlesFormContainer />        
       </div>
