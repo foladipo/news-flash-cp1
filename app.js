eef92eb45dbff4d38bd86d659efd87a9cf588436
Folusho Oladipo <folusho.oladipo@andela.com>
2017-05-01 00:33:58 +0100

    convert file from es5 to es6

--- app.js
+++ app.js
@@ -1,54 +1,39 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import firebaseAdmin from 'firebase-admin';
+import firebaseAdminServiceAccount from './privateFiles/firebaseAdminServiceAccount.json';
+import index from './routes/index';
+import dashboard from './routes/dashboard';
 
-const dotenv = require('dotenv');
 dotenv.config();
 
-const firebaseAdmin = require('firebase-admin');
-const firebaseAdminServiceAccount = require('./privateFiles/firebaseAdminServiceAccount.json');
 const firebaseApp = firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(firebaseAdminServiceAccount),
-  databaseURL: 'https://news-flash-cp1.firebaseio.com',
+  databaseURL: process.env.FIREBASE_DB_URL,
 });
 const firebaseAuth = firebaseApp.auth();
-const firebaseDb = firebaseApp.database();
 
-// NOTE: On ALL routes, always, ALWAYS check that this user is logged in.
-// If not, redirect him/her to the home page. This check is done further below.
-const index = require('./routes/index');
-const dashboard = require('./routes/dashboard');
+const app = express();
 
-const expressApp = express();
+app.set('port', (process.env.PORT || 5000));
 
-expressApp.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, '/dist')));
 
-// Set a dir to expose to public requests. Otherwise, a request to
-// other dirs returns a 403.
-expressApp.use(express.static(path.join(__dirname, '/dist')));
+app.use(cookieParser());
 
-// Use the cookie-parser middleware to parse cookies.
-expressApp.use(cookieParser());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// View engine setup.
-expressApp.set('views', path.join(__dirname, 'views'));
-expressApp.set('view engine', 'ejs');
+app.use(logger('dev'));
 
-expressApp.use(logger('dev'));
-
-// Redirect all non-logged in users to the home page.
-// Otherwise, move on to the route they specified initially.
-// TODO: Add code such that after he/she logs in, a user is
-// taken to the route they were going to before.
-expressApp.all('*', (req, res, next) => {
+app.all('*', (req, res, next) => {
   const idToken = req.cookies.idToken;
   const urlPath = req.path;
 
-  // Check id the idToken is defined and has a value apart from 'null'.
-  // Then check if it is an authentic one i.e can be decoded.
   if (idToken === undefined || idToken === 'null') {
-    // TODO: Maybe add some error message data or something?
     if (urlPath === '/') {
       next();
     } else {
@@ -56,16 +41,13 @@ expressApp.all('*', (req, res, next) => {
     }
   } else {
     firebaseAuth.verifyIdToken(idToken)
-      .then((decodedToken) => {
-        const uid = decodedToken.uid;
-
+      .then(() => {
         if (urlPath === '/') {
           res.redirect('/dashboard');
         } else {
           next();
         }
-      }).catch((error) => {
-        // TODO: Maybe add some error message data or something?
+      }).catch(() => {
         if (urlPath === '/') {
           next();
         } else {
@@ -75,28 +57,23 @@ expressApp.all('*', (req, res, next) => {
   }
 });
 
-// Respond to various routes. 404s are handled further down.
-expressApp.use('/', index);
-expressApp.use('/dashboard', dashboard);
+app.use('/', index);
+app.use('/dashboard', dashboard);
 
-// Catch 404 and forward to error handler.
-expressApp.use((req, res, next) => {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// Error handler.
-expressApp.use((err, req, res, next) => {
-  // Set locals, only providing error in development.
+app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Render the error page.
   res.status(err.status || 500);
   res.render('error');
 });
 
-expressApp.listen(expressApp.get('port'), () => {
-  console.log('Node app is running on port', expressApp.get('port'));
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
 });
