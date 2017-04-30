c13b7aaa510d17381ac59eed32b73451ad770c1c
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-30 14:14:04 +0100

    write code to meet all specs; fix eslint errors

--- src/js/views/Article.js
+++ src/js/views/Article.js
@@ -1,27 +1,34 @@
 import React from 'react';
 
 export default class Article extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const { id, title, description, url, imageUrl, author, publishedAt } = this.props;
     const facebookShareLink = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
     const googleShareLink = `https://plus.google.com/share?url=${url}`;
-    const twitterShareLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(title)}&url=${url}&`;
+    const encodedTitle = encodeURIComponent(title);
+    const twitterShareLink =
+      `https://twitter.com/intent/tweet?text=${encodedTitle}&url=${url}&`;
 
     let publishDate = '';
     if (publishedAt) {
       publishDate = publishedAt.split('T')[0];
     }
     return (
-      <div className="row article quarter-horizontal-whitespace">
+      <div
+        key={id}
+        className="row article quarter-horizontal-whitespace">
         <div className="col-xs-4">
-          <a href={url} target="_blank"><img src={imageUrl} className="img-responsive img-thumbnail" /></a>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            <img
+              className="img-responsive img-thumbnail" src={imageUrl}
+              alt=""
+            />
+          </a>
         </div>
         <div className="col-xs-8">
-          <a href={url} target="_blank"><span className="article-title">{title}</span></a>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            <span className="article-title">{title}</span>
+          </a>
           <div className="">
             <ul className="list-inline">
               <li>At: <b>{publishDate}</b></li>
@@ -32,13 +39,40 @@ export default class Article extends React.Component {
           <div>
             <div className="col-xs-12">
               <div className="btn-group pull-right">
-                <button type="button" className="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button
+                  type="button" className="btn btn-success dropdown-toggle"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                >
                   Share <span className="caret" />
                 </button>
                 <ul className="dropdown-menu">
-                  <li><a href={facebookShareLink} target="_blank">Facebook</a></li>
-                  <li><a href={googleShareLink} target="_blank">Google+</a></li>
-                  <li><a href={twitterShareLink} target="_blank">Twitter</a></li>
+                  <li>
+                    <a
+                      href={facebookShareLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Facebook
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={googleShareLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                    Google+
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={twitterShareLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Twitter
+                    </a>
+                  </li>
                 </ul>
               </div>
             </div>
