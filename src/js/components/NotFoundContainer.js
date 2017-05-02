0b086fc890624840cc3da797e075a8c0f1586d5f
Folusho Oladipo <folusho.oladipo@andela.com>
2017-05-02 14:55:59 +0100

    bug fix: going back to the previous page now works

--- src/js/components/NotFoundContainer.js
+++ src/js/components/NotFoundContainer.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 function NotFoundContainer() {
-  const backBtn = document.getElementById('go-back-btn');
-  backBtn.onclick = (e) => {
+  const goBack = (e) => {
     e.preventDefault();
-    history.back();
+    window.history.back();
   };
 
   return (
@@ -12,7 +11,13 @@ function NotFoundContainer() {
       <div className="col-xs-0 col-md-1" />
       <div className="col-xs-12 col-md-10">
         <div className="full-horizontal-whitespace" />
-        <button id="go-back-btn" className="btn btn-success btn-lg center-block">Go Back</button>
+        <button
+          id="go-back-btn"
+          onClick={goBack}
+          className="btn btn-success btn-lg center-block"
+        >
+          Go Back
+        </button>
         <div className="quarter-horizontal-whitespace" />
         <img className="img-responsive center-block" alt="Page not found. Please go back." src="https://res.cloudinary.com/worldgeek/image/upload/v1493668469/404-page-not-found.jpg" />
       </div>
