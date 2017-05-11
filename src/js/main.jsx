d765a37ec6975d192047af79768fd4569b41b355
Folusho Oladipo <folusho.oladipo@andela.com>
2017-05-11 18:18:47 +0100

    remove the use of two scripts

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RootContainer from './components/RootContainer';
 
-// Styles and JS code for the rendered document.
-
+// Styles for the rendered document.
 // eslint-disable-next-line no-unused-vars
 import mainStyles from '../css/style.scss';
-
-// eslint-disable-next-line no-unused-vars
-import * as homePage from './util/homePage';
-
-// eslint-disable-next-line no-unused-vars
-import * as dashboard from './util/dashboard';
 
 ReactDOM.render(<RootContainer />, document.getElementById('root-container'));
