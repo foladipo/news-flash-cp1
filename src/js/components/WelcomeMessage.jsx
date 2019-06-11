3776d21f607ce3923dd60bb68b1435127bbb2f76
Folusho Oladipo <folusho.oladipo@andela.com>
2019-06-11 12:59:53 +0100

    improve ui styling

--- src/js/components/WelcomeMessage.jsx
+++ src/js/components/WelcomeMessage.jsx
@@ -39,7 +39,7 @@ export default class WelcomeMessageContainer extends React.Component {
           alt="Welcome to the dashboard!"
           src="https://res.cloudinary.com/worldgeek/image/upload/c_scale,w_400/v1493551217/welcome-img_pbxehe.png"
         />
-        <h1 className="display-4 text-center bg-light-blue">Welcome to your dashboard</h1>
+        <h1 className="display-4 text-center bg-light-blue">Welcome to the dashboard!</h1>
         <p className="lead text-center">
           To read news articles from your desired source, please
            use the form in the navigation bar.
